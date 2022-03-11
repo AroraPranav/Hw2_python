80b10b2bd9ac1a8952f4415774db13d42997e987
Pranav Arora <89612594+AroraPranav@users.noreply.github.com>
2022-03-10 21:25:19 -0800

    Add files via upload

--- timeit.py
+++ timeit.py
@@ -1,4 +1,3 @@
-'''
 import time
 
 
@@ -18,22 +17,3 @@ def print_smthng():
 
 
 print_smthng()
-'''
-
-import time
-from functools import wraps
-
-
-# function to gerate execution time
-def calculate_time(func):
-    @wraps(func)
-    def _give_time(*args, **kwargs):
-        start_time = int((round(time() * 1000) / 1000) % 60)
-        try:
-            return func(*args, **kwargs)
-        finally:
-            end_time = int((round(time() * 1000) / 1000) % 60)
-            execution_time = end_time - start_time
-            print(f"EXACTLY  {execution_time if execution_time > 0 else 0} second")
-
-    return _give_time
